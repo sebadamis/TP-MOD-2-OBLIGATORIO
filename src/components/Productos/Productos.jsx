9a6f23b520426a809969d2b9bbe7d3c82e26effd
Sebastián Damis <sebastian.damis@gmail.com>
2025-05-02 19:02:37 -0300

    4th commit

--- src/components/Productos/Productos.jsx
+++ src/components/Productos/Productos.jsx
@@ -3,13 +3,14 @@ import "./Productos.css"
 import BotonComprar from '../BotonCompra/BotonCompra'
 import { NavLink } from 'react-router'
 
-export const Productos2 = ({id, titulo, img, precioAntes, precioFinal}) => {
+export const Productos2 = ({id, titulo, img, precioAntes, precioFinal, descuento}) => {
     return (
     <>
         <body className='body-card'>
             <div className="card-container">
                 <div className="card">
                     <div className="imgBox">
+                        <span className='discount'>{descuento}%</span>
                         <img
                         src={img}
                         alt={id}
@@ -19,7 +20,7 @@ export const Productos2 = ({id, titulo, img, precioAntes, precioFinal}) => {
                     <div className="contentBox">
                         <h3>{titulo}</h3>
                         <h2 className="price">
-                        {precioFinal}<small>  {precioAntes}</small> $
+                        <span className='preciof'>${precioFinal}</span><span className='precioa'>${precioAntes}</span>
                         </h2>
                         <div className="botones-card">
                             <button className="button"><BotonComprar type="button" >COMPRAR</BotonComprar></button>
