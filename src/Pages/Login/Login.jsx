0e6d706a7fefc14d7011e3217fd023e8276fa7f9
Sebastián Damis <sebastian.damis@gmail.com>
2025-05-03 13:56:47 -0300

    11th commit

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -11,12 +11,12 @@ function Login() {
                     <h2>Login</h2>
                     <form>
                         <div className="user-box">
-                        <input type="text" name="" required="" />
-                        <label>Username</label>
-                        </div>
-                        <div className="user-box">
-                        <input type="password" name="" required="" />
-                        <label>Password</label>
+                            <input type="text" name="" required="" />
+                            <label className='user-name'>Nombre Usuario</label>
+                            </div>
+                            <div className="user-box">
+                            <input type="password" name="" required="" />
+                            <label className='password-text'>Password</label>
                         </div>
                     </form>
                 </div>
